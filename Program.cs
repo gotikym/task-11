53f0be88e5917681eb9a40d49b74f8140cb1c799
One <One@DESKTOP-MIB22JC>
2022-04-26 22:52:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxHealth = 100;
+            int treatment = 7;
+
+            for (int i = 5; i < maxHealth; i += treatment)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
